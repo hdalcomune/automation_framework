f426fbb8580751b0af4ceefcc51f6a07a9caafa4
Henrique Dalcomune <henrique.dalcomune@macys.com>
2015-09-16 09:35:45 -0300

    Removed yml file data. Refactored setup_browser method.

--- features/support/browser.rb
+++ features/support/browser.rb
@@ -1,20 +1,13 @@
 module Browser
 
 	def self.setup_browser
-		browser_name = ENV['BROWSER']
-		browser_name = 'firefox' if browser_name == nil
+		ENV['BROWSER'] = 'firefox' if ENV['BROWSER'].nil?
+		browser_name = ENV['BROWSER'].downcase.to_sym
 
-		case browser_name.downcase
-			when 'chrome'
-				browser=Selenium::WebDriver.for :chrome	
-			when 'ie'
-				browser=Selenium::WebDriver.for :ie
-			when 'safari'
-				browser=Selenium::WebDriver.for :safari
-			when 'firefox'
-				browser=Selenium::WebDriver.for :firefox
-			else
-				raise "Invalid browser"
+    if(browser_name == :chrome)
+    	browser=Selenium::WebDriver.for :chrome
+    else
+			browser=Selenium::WebDriver.for browser_name
 		end
 	end
 end
